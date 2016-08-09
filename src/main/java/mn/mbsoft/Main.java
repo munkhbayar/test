02e2a28c3279180d20946bcfc48a6e89be87a337
Munkhbayar B <munkhbayar.b@gmail.com>
2016-08-09 15:52:17 +0900

    Second commit

---
+++ src/main/java/mn/mbsoft/Main.java
@@ -0,0 +1,10 @@
+package mn.mbsoft;
+
+/**
+ * Created by MTLT on 8/9/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
